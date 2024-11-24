b07f857db837be12a705d98847770e8d79803097
TurmaMoreDevs <moredevs2blumenaujava@gmail.com>
2024-11-24 18:31:01 -0300

    commit 2

--- IntegracaoGitEclipse/src/exemplo/Teste.java
+++ IntegracaoGitEclipse/src/exemplo/Teste.java
@@ -6,6 +6,7 @@ public class Teste {
 		// TODO Auto-generated method stub
 		System.out.println("ALTERADO DO eclipse");
 		System.out.println("ALTERADO DO GitHub");
+		System.out.println("segundo commit");
 	}
 
 }
