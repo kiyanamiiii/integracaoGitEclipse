5b5f7458af651254ebc2db7e99d22f4e80982552
TurmaMoreDevs <moredevs2blumenaujava@gmail.com>
2024-11-24 13:27:53 -0300

    Update Teste.java
    alterado do github

--- IntegracaoGitEclipse/src/exemplo/Teste.java
+++ IntegracaoGitEclipse/src/exemplo/Teste.java
@@ -5,6 +5,7 @@ public class Teste {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("ALTERADO DO eclipse");
+		System.out.println("ALTERADO DO GitHub");
 	}
 
 }
